1df98017b9c39517174145b4392fd8731afaa993
jiangyi <1114219654@qq.com>
2021-05-19 19:21:08 +0800

    合并分支

--- demo.js
+++ demo.js
@@ -163,7 +163,7 @@ window.onload=function(){
         （以当前分支为主合并代码，注意另一个分支的代码不会被修改成和这个分支一样
             例：合并master和xixi，两个分支都对demo.html做了修改，发生冲突，
             在master分支下使用git merge --abort 命令，即将demo.html改为master下的
-            demo.html）
+            demo.html，并不用在此提交，直接推送就行。注意：xixi分支下的demo.html不会被修改）
 
     */
 
